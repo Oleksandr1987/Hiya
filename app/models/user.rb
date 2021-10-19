2696d50a900764f1a4810a63926ba3675bb24287
Oleksansr1987 <solovij1987@gmail.com>
2021-10-19 15:11:30 +0300

    Update file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true
-   validates :name, presence: true
+  format: { with: VALID_EMAIL_REGEX }
+   validates :name, presence: true, length: { maximum: 50 }
 end
